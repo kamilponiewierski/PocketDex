6e720e53e7ffb321e39856c2974cacc5a6f889a6
kponiewierski <vaskkr@gmail.comclear>
2020-02-06 23:02:37 +0100

    renamed

--- app/src/main/java/com/example/pokemon_api_example/PokemonApi.kt
+++ app/src/main/java/com/example/pokemon_api_example/PokemonApi.kt
@@ -6,7 +6,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Url
 
-interface GetPokemon {
+interface PokemonApi {
     @GET ("/api/v2/pokemon")
     fun getPage(): Call<PokemonList>?
 
