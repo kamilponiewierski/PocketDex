0ae3d01d57523d2b729d92c024891d541d57777e
kponiewierski <vaskkr@gmail.comclear>
2020-02-07 23:56:39 +0100

    filtering feature added

--- app/src/main/java/com/example/pokemon_api_example/adapters/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokemon_api_example/adapters/PokemonAdapter.kt
@@ -1,53 +1,47 @@
 package com.example.pokemon_api_example.adapters
 
 import android.content.Context
-import android.provider.Settings.Global.getString
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.lifecycle.ViewModel
+import android.widget.*
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.pokemon_api_example.R
-import com.example.pokemon_api_example.dto.PokemonDTO
 import com.example.pokemon_api_example.dto.PokemonFullDTO
-import com.example.pokemon_api_example.viewmodels.MainActivityViewModel
+import java.util.*
 
-class PokemonAdapter(context : Context, viewModel : MainActivityViewModel) : BaseAdapter()
+class PokemonAdapter(context : Context, pokemons : List<PokemonFullDTO>) : BaseAdapter(), Filterable
 {
-    var pokemonList : List<PokemonFullDTO> = viewModel.getPokemonList()
     private val mContext : Context = context
+    private val mOriginalPokemonData : List<PokemonFullDTO> = pokemons
+    private var mFilteredPokemonData : List<PokemonFullDTO> = pokemons
 
-    private val pokemon = pokemonList
-
-
-
+    @ExperimentalStdlibApi
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val layoutInflater = LayoutInflater.from(mContext)
         val row = layoutInflater.inflate(R.layout.row_main, parent, false)
 
         val nameTextView = row.findViewById<TextView>(R.id.name)
-        nameTextView.text = pokemon[position].name.capitalize()
+        nameTextView.text = mFilteredPokemonData[position].name.capitalize(Locale.ENGLISH)
 
         val sprite = row.findViewById<ImageView>(R.id.pokemonSprite)
         if (convertView != null) {
             Glide
                 .with(convertView)
-                .load(pokemon[position].sprites.get("front_default"))
+                .load(mFilteredPokemonData[position].sprites["front_default"])
+                .apply(RequestOptions().placeholder(R.drawable.pokeball_placeholder).fitCenter())
                 .into(sprite)
-            Log.i("sprite", "Loaded a sprite")
         }
 
         val positionTextView = row.findViewById<TextView>(R.id.url)
-        positionTextView.text ="Pokedex # ".plus(pokemon[position].id)
+        positionTextView.text = mContext.getString(R.string.pokedex_number).plus(mFilteredPokemonData[position].id)
+
         return row
     }
 
     override fun getItem(position: Int): Any {
-        return position
+        return mFilteredPokemonData[position]
     }
 
     override fun getItemId(position: Int): Long {
@@ -55,7 +49,29 @@ class PokemonAdapter(context : Context, viewModel : MainActivityViewModel) : Bas
     }
 
     override fun getCount(): Int {
-        return pokemon.size
+        return mFilteredPokemonData.size
     }
 
+    override fun getFilter(): Filter {
+        return object : Filter()
+        {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val queryString : String = constraint.toString().toLowerCase(Locale.ROOT)
+                val filterResults = FilterResults()
+
+                filterResults.values = if (queryString.isEmpty())
+                    mOriginalPokemonData
+                else
+                    mOriginalPokemonData.filter { it.name.startsWith(queryString) }
+
+                return filterResults
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                mFilteredPokemonData = results?.values as List<PokemonFullDTO>
+                mFilteredPokemonData = mFilteredPokemonData.sortedBy { it.id }
+                notifyDataSetChanged()
+            }
+        }
+    }
 }
